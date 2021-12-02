5ff58d051a32f2b901fda55d96e529919849c684
Zor Max <okocadere@gmail.com>
2021-12-02 11:48:20 +0300

    end of lesson 112

--- angular-essentials-project/src/app/app-routing.module.ts
+++ angular-essentials-project/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
+
+const routes = [
+  {
+    path: 'characters', component: TabsComponent, children: [
+      { path: '', redirectTo: 'all', pathMatch: 'full' },
+      { path: ':side', component: ListComponent }
+    ]
+  },
+  {
+    path: 'new-character', loadChildren: () => import('./create-character/create-character.module')
+      .then(m => m.CreateCharacterModule)
+  },
+  { path: '**', redirectTo: '/characters' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-essentials-project/src/app/app.module.ts
+++ angular-essentials-project/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateCharacterComponent } from './create-character/create-character.component';
 import { HeaderComponent } from './header/header.component';
 import { ItemComponent } from './item/item.component';
 import { ListComponent } from './list/list.component';
@@ -13,33 +11,18 @@ import { StarWarsService } from './star-wars.service';
 import { TabsComponent } from './tabs/tabs.component';
 
 
-
-const routes = [
-  {
-    path: 'characters', component: TabsComponent, children: [
-      { path: '', redirectTo: "all", pathMatch:"full" },
-      { path: ':side', component: ListComponent },
-      { path: '**', redirectTo: "asd" }
-    ]
-  },
-  { path: 'new-character', component: CreateCharacterComponent },
-  { path: '**', redirectTo: "characters" }
-]
-
 @NgModule({
   declarations: [
     AppComponent,
     TabsComponent,
     ListComponent,
     ItemComponent,
-    CreateCharacterComponent,
     HeaderComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
-    FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [StarWarsService, LogService],
   bootstrap: [AppComponent]
